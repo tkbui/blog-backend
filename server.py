840f1167dfce3ae59c97746c19770122451cef9d
T.K. Bui <62676217+tkbui@users.noreply.github.com>
2024-01-30 01:04:40 -0800

    Add routes for getting games and songs

--- server.py
+++ server.py
@@ -1,16 +1,45 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import json
+
+# https://www.geeksforgeeks.org/read-json-file-using-python/
+# https://www.geeksforgeeks.org/with-statement-in-python/
+with open('./utils/db/games.json') as games_data:
+  games = json.load(games_data)
+
+with open('./utils/db/songs.json') as songs_data:
+  songs = json.load(songs_data)
 
 # create Flask app instance
 app = Flask(__name__)
 CORS(app)
 
 # example route
-@app.route('/api/home', methods=["GET"])
+@app.route('/api/home', methods=['GET'])
 def return_home():
     return jsonify({
       'message': 'Hello World!'
     })
+
+# /api/games
+@app.route('/api/games', methods=['GET'])
+def get_games():
+    """
+    Get the games and their data from games.json.
+
+    @returns: Games <object>
+    """
+    return games
+
+# /api/songs
+@app.route('/api/songs', methods=['GET'])
+def get_songs():
+    """
+    Get the songs and their data from songs.json.
+    
+    @returns: Games <object>
+    """
+    return songs
 
 # way to run the app
 if __name__ == '__main__':
